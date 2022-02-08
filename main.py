4d54f7a74521ceb2209eedf33cae7777e93cbabf
Andrew <x.and.i.rew.e@gmail.com>
2022-02-09 05:46:11 +1100

    added argparse flag for starting flask server, updated replit config

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+import argparse
 import asyncio
 from threading import Thread
 
 from src.digibot.controller import start_discord_server
 from src.server import start_flask_server
 
+parser = argparse.ArgumentParser("DigiBot")
+parser.add_argument(
+    "-f",
+    "--flask",
+    action="store_true",
+    help="Start a Flask server on a separate thread",
+)
+args = parser.parse_args()
+
 if __name__ == "__main__":
     # start Flask server
-    # t = Thread(target=start_flask_server)
-    # t.start()
+    if args.flask:
+        t = Thread(target=start_flask_server)
+        t.start()
     # start DigiBot
     loop = asyncio.get_event_loop()
     loop.run_until_complete(start_discord_server())
