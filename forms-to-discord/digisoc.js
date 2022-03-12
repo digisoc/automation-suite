2aceb91c6c9cdd5fec1ddbee1e333702856a35d5
Andrew <x.and.i.rew.e@gmail.com>
2022-03-12 14:52:29 +1100

    fix: attendance form short url link

--- forms-to-discord/digisoc.js
+++ forms-to-discord/digisoc.js
@@ -77,7 +77,7 @@ const submitPost = (e) => {
       responses: registrationSheet.getUrl(),
     },
     attendance: {
-      public: attendanceForm.shortenFormUrl(registrationForm.getPublishedUrl()),
+      public: attendanceForm.shortenFormUrl(attendanceForm.getPublishedUrl()),
       editor: attendanceForm.getEditUrl(),
       responses: attendanceSheet.getUrl(),
     },
